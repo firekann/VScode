dfe940d1e89d2b93acf6189b633d8170d2799727
Homin Lee <leehomin118@gmail.com>
2020-07-29 17:02:29 +0900

    ㄷㄷㄷㅈ AC

--- PS/UCPC/2020/예선/D/D.cpp
+++ PS/UCPC/2020/예선/D/D.cpp
@@ -2,6 +2,54 @@
 #pragma GCC optimize("Ofast")
 #pragma GCC optimize ("unroll-loops")
 #include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define X first
+#define Y second
+#define pb push_back
+#define sz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+using namespace std;
+typedef long long ll;
+const int dx[] = { -1, 0, 1, 0, -1, 1, 1, -1 };
+const int dy[] = { 0, 1, 0, -1, 1, 1, -1, -1 };
+
+const int NODE=3e5+10;
+vector<int> edge[NODE];
+ll D, G;
+
+int main(){
+    fastio
+    int n;
+    cin>>n;
+    vector<pii> mid_edge;
+    for(int i=0; i<n-1; i++){
+        int a, b;
+        cin>>a>>b;
+        edge[a].pb(b);
+        edge[b].pb(a);
+        mid_edge.pb({a, b});
+    }
+    for(int i=1; i<=n; i++){
+        ll tmp=sz(edge[i]);
+        G+=tmp*(tmp-1)*(tmp-2)/6;
+    }
+    for(auto itr:mid_edge){
+        ll tmp=(sz(edge[itr.X])-1)*(sz(edge[itr.Y])-1);
+        D+=tmp;
+    }
+    if(D==G*3) cout<<"DUDUDUNGA";
+    else if(D>G*3) cout<<"D";
+    else cout<<"G";
+}
+
+/*
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize ("unroll-loops")
+#include <bits/stdc++.h>
 #define X first
 #define Y second
 #define pb push_back
@@ -56,3 +104,4 @@ int main(){
     else if(D>G*3) cout<<"D";
     else cout<<"G";
 }
+*/
