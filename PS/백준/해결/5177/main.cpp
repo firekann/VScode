1bc0303cac5d5803b8a56993772156b332e5830b
Homin Lee <leehomin118@gmail.com>
2020-07-30 14:16:23 +0900

    string 모음 추가

--- PS/백준/해결/5177/main.cpp
+++ PS/백준/해결/5177/main.cpp
@@ -16,7 +16,32 @@ typedef long long ll;
 const int dx[] = { -1, 0, 1, 0, -1, 1, 1, -1 };
 const int dy[] = { 0, 1, 0, -1, 1, 1, -1, -1 };
 
+void ltrim(string &s){s.erase(s.begin(), find_if(all(s), [](int ch){return !isspace(ch);}));}
+void rtrim(string &s){s.erase(find_if(s.rbegin(), s.rend(), [](int ch){return !isspace(ch);}).base(), s.end());}
+void trim(string &s){rtrim(s),ltrim(s);}
+void lowwercase(string &s){transform(s.begin(), s.end(), s.begin(),[](unsigned char c){ return std::tolower(c);});}
+void replace(string &s){for(auto &i:s){if(i=='{'||i=='[') i='(';if(i=='}'||i==']') i=')';if(i==';') i=',';}}
+void del_space(string &s){
+    int idx=-1, n=sz(s);
+    bool mk=false;
+    for(int i=0; i<n; i++){
+        if(mk&&s[i]==' ') s[i]='*';
+        else if(s[i]==' ') mk=true;
+        else if(mk) mk=false;
+    }
+    s.erase(remove(all(s), '*'), s.end());
+    n=sz(s);
+    for(int i=0; i<n; i++){
+        if(s[i]=='('||s[i]==')'||s[i]==','||s[i]=='.'||s[i]==':'){
+            if(i>0&&s[i-1]==' ') s[i-1]='*';
+            if(i<n-1&&s[i+1]==' ') s[i+1]='*';
+        }
+    }
+    s.erase(remove(all(s), '*'), s.end());
+}
+
 int main(){
+    fastio
     int n;
     cin>>n;
     cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
@@ -24,61 +49,11 @@ int main(){
         string a, b;
         getline(cin, a);
         getline(cin, b);
-        int alen=sz(a);
-        int blen=sz(b);
-        int cnt=0;
-        bool flag=true;
-        for(int i=0; i<alen; i++){
-            if(a[i]==' '){
-                if(b[cnt]!=' '){
-                    cout<<a[i]<<"===\n";
-                    cout<<"Data Set "<<T<<": not equal\n\n";
-                    flag=false;
-                    break;
-                }
-                for(;b[cnt]==' '; cnt++);
-                for(;a[i]==' ';i++);
-                i--;
-            }
-            else if(a[i]=='('||a[i]=='{'||a[i]=='['){
-                if(b[cnt]=='('||b[cnt]=='{'||b[cnt]=='[') cnt++;
-                else{
-                    cout<<a[i]<<"===\n";
-                    cout<<"Data Set "<<T<<": not equal\n\n";
-                    flag=false;
-                    break;
-                }
-            }
-            else if(a[i]==')'||a[i]=='}'||a[i]==']'){
-                if(b[cnt]==')'||b[cnt]=='}'||b[cnt]==']') cnt++;
-                else{
-                    cout<<a[i]<<"===\n";
-                    cout<<"Data Set "<<T<<": not equal\n\n";
-                    flag=false;
-                    break;
-                }
-            }
-            else if(a[i]==','||a[i]==';'){
-                if(b[cnt]==','||b[cnt]==';') cnt++;
-                else{
-                    cout<<a[i]<<"===\n";
-                    cout<<"Data Set "<<T<<": not equal\n\n";
-                    flag=false;
-                    break;
-                }
-            }
-            else{
-                a[i]=toupper(a[i]);
-                b[cnt]=toupper(b[cnt]);
-                if(a[i]==b[cnt]) cnt++;
-                else{
-                    cout<<a[i]<<"===\n";
-                    cout<<"Data Set "<<T<<": not equal\n\n";
-                    flag=false;
-                    break;
-                }
-            }
-        }
-        if(flag) cout<<"Data Set "<<T<<": equal\n\n";
+        trim(a), trim(b);
+        lowwercase(a), lowwercase(b);
+        replace(a), replace(b);
+        del_space(a), del_space(b);
+        if(a==b) cout<<"Data Set "<<T<<": equal\n\n";
+        else cout<<"Data Set "<<T<<": not equal\n\n";
     }
 }
