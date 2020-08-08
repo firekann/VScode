7d54c44cb081bfd3995da1a1a86d9778d4af23ae
Homin Lee <leehomin118@gmail.com>
2020-08-08 17:18:31 +0900

    ZeroForces #Div.3 001 업솔빙

--- PS/Zeropage/ZeroForces #Div.3 001/G/main.cpp
+++ PS/Zeropage/ZeroForces #Div.3 001/G/main.cpp
@@ -11,26 +11,26 @@
 
 using namespace std;
 typedef long long ll;
-const int dx[] = { -1, 0, 1, 0, -1, 1, 1, -1 };
-const int dy[] = { 0, 1, 0, -1, 1, 1, -1, -1 };
+const int dx[] ={ -1, 0, 1, 0, -1, 1, 1, -1 };
+const int dy[] ={ 0, 1, 0, -1, 1, 1, -1, -1 };
 
-struct node{
+struct node {
     int x, y1, y2, end;
 
-    bool operator<(const node &p)const{
-        if(x==p.x) return end>p.end;
+    bool operator<(const node &p)const {
+        if (x==p.x) return end>p.end;
         return x<p.x;
     }
 };
 
-const int treeMAX=1e5, half=5e4;
-int seg[30000];
-int lzy[30000];
+const int treeMAX=1e5, treehalf=5e4;
+int seg[400400];
+int lzy[400400];
 vector<node> vp;
 
-int update(int l, int r, int node, int i, int j, int v){
-    if(j<l||r<i) return lzy[node]?r-l+1:seg[node];
-    if(i<=l&&r<=j){
+int update(int l, int r, int node, int i, int j, int v) {
+    if (j<l||r<i) return lzy[node]?r-l+1:seg[node];
+    if (i<=l&&r<=j) {
         lzy[node]+=v;
         return lzy[node]?r-l+1:seg[node];
     }
@@ -38,20 +38,20 @@ int update(int l, int r, int node, int i, int j, int v){
     return lzy[node]?r-l+1:seg[node];
 }
 
-int main(){
-    fastio
+int main() {
+    fastio;
     int n;
     cin>>n;
-    for(int i=0; i<n; i++){
+    for (int i=0; i<n; i++) {
         int x1, x2, y1, y2;
         cin>>x1>>y1>>x2>>y2;
-        vp.push_back({x1, y1+half, y2+half-1, 1});
-        vp.push_back({x2, y1+half, y2+half-1, -1});
+        vp.push_back({ x1, y1+treehalf, y2+treehalf-1, 1 });
+        vp.push_back({ x2, y1+treehalf, y2+treehalf-1, -1 });
     }
     sort(all(vp));
     int x=vp[0].x;
     ll ans=0;
-    for(auto itr:vp){
+    for (auto &itr:vp) {
         ans+=(itr.x-x)*seg[1];
         update(0, treeMAX, 1, itr.y1, itr.y2, itr.end);
         x=itr.x;
