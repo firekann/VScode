0b3141a36349e66cf0b7b73772de7252bbc44ee9
Homin Lee <leehomin118@gmail.com>
2020-08-08 15:34:21 +0900

    added extension and test

--- PS/ChAOS/2020/카카오 코테 스터디/크레인 인형뽑기 게임/main.cpp
+++ PS/ChAOS/2020/카카오 코테 스터디/크레인 인형뽑기 게임/main.cpp
@@ -2,7 +2,7 @@
 #pragma GCC optimize("Ofast")
 #pragma GCC optimize ("unroll-loops")
 #include <bits/stdc++.h>
-#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr)
 #define X first
 #define Y second
 #define pb push_back
@@ -13,28 +13,28 @@
 
 using namespace std;
 typedef long long ll;
-const int dx[] = { -1, 0, 1, 0, -1, 1, 1, -1 };
-const int dy[] = { 0, 1, 0, -1, 1, 1, -1, -1 };
+const int dx[] ={ -1, 0, 1, 0, -1, 1, 1, -1 };
+const int dy[] ={ 0, 1, 0, -1, 1, 1, -1, -1 };
 
 int solution(vector<vector<int>> board, vector<int> moves) {
     int answer = 0;
-    
+
     return answer;
 }
 
-int main(){
-    fastio
+int main() {
+    fastio;
     int n;
     cin>>n;
     vector<vector<int>> vv;
     vv.resize(n);
-    for(auto &i:vv){
+    for (auto &i:vv) {
         i.resize(n);
-        for(auto &j:i) cin>>j;
+        for (auto &j:i) cin>>j;
     }
     int m;
     cin>>m;
     vector<int> v(m);
-    for(auto &i:v) cin>>i;
+    for (auto &i:v) cin>>i;
     cout<<solution(vv, v);
 }
--- test/test.cpp
+++ test/test.cpp
@@ -2,7 +2,7 @@
 #pragma GCC optimize("Ofast")
 #pragma GCC optimize ("unroll-loops")
 #include <bits/stdc++.h>
-#define fastio ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
 #define X first
 #define Y second
 #define pb push_back
@@ -13,10 +13,10 @@
 
 using namespace std;
 typedef long long ll;
-const int dx[] = { -1, 0, 1, 0, -1, 1, 1, -1 };
-const int dy[] = { 0, 1, 0, -1, 1, 1, -1, -1 };
+const int dx[] ={ -1, 0, 1, 0, -1, 1, 1, -1 };
+const int dy[] ={ 0, 1, 0, -1, 1, 1, -1, -1 };
 
-int main(){
-    fastio
+int main() {
+    fastio;
     cout<<"test";
 }
