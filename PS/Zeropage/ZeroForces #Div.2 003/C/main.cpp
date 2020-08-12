38c6859815d6b945b6a9d1601dd1d1e01936d1f0
vulcan <leehomin118@gmail.com>
2020-08-12 22:09:07 +0900

    update ZeroForces #Div.2 003

--- PS/Zeropage/ZeroForces #Div.2 003/C/main.cpp
+++ PS/Zeropage/ZeroForces #Div.2 003/C/main.cpp
@@ -17,7 +17,27 @@ typedef long long ll;
 const int dx[] ={ -1, 0, 1, 0, -1, 1, 1, -1 };
 const int dy[] ={ 0, 1, 0, -1, 1, 1, -1, -1 };
 
+vector<int> v;
+
+int f(int l, int r) {
+    int tmp=1;
+    for (int i=l; i<=r; i++) tmp*=v[i];
+    return tmp;
+}
+
 int main() {
     fastio;
-
+    int n;
+    cin>>n;
+    v.resize(n+1);
+    for (int i=1; i<=n; i++) cin>>v[i];
+    int ans=0;
+    for (int i=1; i<n; i++) {
+        for (int j=i+1; j<n; j++) {
+            for (int k=j+1; k<n; k++) {
+                ans=max(ans, f(1, i)+f(i+1, j)+f(j+1, k)+f(k+1, n));
+            }
+        }
+    }
+    cout<<ans;
 }
