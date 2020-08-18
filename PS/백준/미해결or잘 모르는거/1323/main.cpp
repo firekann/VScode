e1be180d4df66c380e1115cb1997083ca2595ed1
Homin Lee <leehomin118@gmail.com>
2020-08-19 01:09:00 +0900

    1323

---
+++ PS/백준/미해결or잘 모르는거/1323/main.cpp
@@ -0,0 +1,39 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+#define fastio                   \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr)
+#define X first
+#define Y second
+#define pb push_back
+#define sz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define mid ((l + r) >> 1)
+
+using namespace std;
+typedef long long ll;
+const int dx[] = {-1, 0, 1, 0, -1, 1, 1, -1};
+const int dy[] = {0, 1, 0, -1, 1, 1, -1, -1};
+
+int main()
+{
+    fastio;
+    ll n, k;
+    cin >> n >> k;
+    string s = to_string(n);
+    for (int i = 1; n <= (ll)1e9; i++)
+    {
+        if (n % k == 0)
+        {
+            cout << i;
+            return 0;
+        }
+        n = stoll(to_string(n) + s);
+    }
+    cout << "-1";
+}
