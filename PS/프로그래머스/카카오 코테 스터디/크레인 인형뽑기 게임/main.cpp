1122d4af709c837f5706c8b8688cd0db97ed30e9
Homin Lee <leehomin118@gmail.com>
2020-09-22 12:41:25 +0900

    크레인 인형뽑기 게임 ac

--- PS/프로그래머스/카카오 코테 스터디/크레인 인형뽑기 게임/main.cpp
+++ PS/프로그래머스/카카오 코테 스터디/크레인 인형뽑기 게임/main.cpp
@@ -21,7 +21,37 @@ const int dy[] = { 0, 1, 0, -1, 1, 1, -1, -1 };
 int solution(vector<vector<int>> board, vector<int> moves)
 {
     int answer = 0;
-
+    int st_size = sz(board);
+    stack<int> st_board[st_size];
+    for (int i = 0; i < st_size; i++) {
+        for (int j = st_size - 1; j >= 0; j--) {
+            if (board[j][i] == 0)
+                break;
+            st_board[i].push(board[j][i]);
+        }
+    }
+    stack<int> st;
+    int cnt = 0;
+    for (auto i : moves) {
+        int tmp;
+        if (!st_board[i - 1].empty()) {
+            tmp = st_board[i - 1].top();
+            st_board[i - 1].pop();
+        } else {
+            continue;
+        }
+        if (!st.empty()) {
+            if (tmp == st.top()) {
+                cnt += 2;
+                st.pop();
+            } else {
+                st.push(tmp);
+            }
+        } else {
+            st.push(tmp);
+        }
+    }
+    answer = cnt;
     return answer;
 }
 
@@ -30,8 +60,7 @@ int main()
     fastio;
     int n;
     cin >> n;
-    vector<vector<int>> vv;
-    vv.resize(n, vector<int>(n));
+    vector<vector<int>> vv(n, vector<int>(n));
     for (auto& i : vv)
         for (auto& j : i)
             cin >> j;
