5b1da6b56eb821ba952f83201e6e07a81389d1ea
Homin Lee <leehomin118@gmail.com>
2021-09-07 22:01:24 +0900

    add operator+ & operator* & bugfix

--- txt/inf_float.cpp
+++ txt/inf_float.cpp
@@ -30,22 +30,11 @@ private:
     unsigned int length;
     unsigned int precision_position;
     bool thesign;
-    void ltrim(std::string& s)
-    {
-        s.erase(s.begin(), find_if(s.begin(), s.end(), [](int ch) { return (ch == '0' ? false : true); }));
-    }
-    void rtrim(std::string& s)
-    {
-        s.erase(find_if(s.rbegin(), s.rend(), [](int ch) { return (ch == '0' ? false : true); }).base(), s.end());
-    }
-    void trim(std::string& s)
-    {
-        rtrim(s);
-        ltrim(s);
-    }
 
 public:
     inf_float();
+    inf_float(int);
+    inf_float(long long);
     inf_float(float);
     inf_float(double);
     inf_float(long double);
@@ -53,10 +42,12 @@ public:
     inf_float(std::string);
     inf_float(const inf_float&);
     ~inf_float();
+    friend void ltrim(std::string& s);
+    friend void rtrim(std::string& s);
+    friend void trim(std::string& s);
     string getstr();
     inf_float& operator=(const inf_float&);
     friend void FFT(std::vector<std::complex<double>>&, bool);
-    friend std::pair<inf_float, inf_float> divmod(const inf_float&, const inf_float&);
     friend bool operator==(const inf_float&, const inf_float&);
     friend bool operator!=(const inf_float&, const inf_float&);
     friend bool operator>(const inf_float&, const inf_float&);
@@ -67,22 +58,16 @@ public:
     friend inf_float operator-(const inf_float&, const inf_float&);
     friend inf_float operator*(const inf_float&, const inf_float&);
     friend inf_float operator/(const inf_float&, const inf_float&);
-    friend inf_float operator%(const inf_float&, const inf_float&);
     friend inf_float operator+(const inf_float&);
     friend inf_float operator-(const inf_float&);
     friend inf_float& operator++(inf_float&);
     friend inf_float operator++(inf_float&, int);
     friend inf_float& operator--(inf_float&);
     friend inf_float operator--(inf_float&, int);
-    friend inf_float operator>>(const inf_float&, const inf_float&);
-    friend inf_float operator<<(const inf_float&, const inf_float&);
     friend inf_float& operator+=(inf_float&, const inf_float&);
     friend inf_float& operator-=(inf_float&, const inf_float&);
     friend inf_float& operator*=(inf_float&, const inf_float&);
     friend inf_float& operator/=(inf_float&, const inf_float&);
-    friend inf_float& operator%=(inf_float&, const inf_float&);
-    friend inf_float& operator>>=(inf_float&, const inf_float&);
-    friend inf_float& operator<<=(inf_float&, const inf_float&);
     friend inf_float abs(const inf_float&);
     friend std::ostream& operator<<(std::ostream&, const inf_float&);
     friend std::istream& operator>>(std::istream&, inf_float&);
@@ -91,8 +76,25 @@ public:
 int main()
 {
     fastio;
+    inf_float a, b;
+    cin >> a >> b;
+    cout << a + b << "\n";
+    cout << a * b << "\n";
 }
 
+void ltrim(std::string& s)
+{
+    s.erase(s.begin(), find_if(s.begin(), s.end(), [](int ch) { return (ch == '0' ? false : true); }));
+}
+void rtrim(std::string& s)
+{
+    s.erase(find_if(s.rbegin(), s.rend(), [](int ch) { return (ch == '0' ? false : true); }).base(), s.end());
+}
+void trim(std::string& s)
+{
+    rtrim(s);
+    ltrim(s);
+}
 string inf_float::getstr()
 {
     return this->digits;
@@ -104,8 +106,10 @@ inf_float::inf_float()
     this->precision_position = 0;
     this->thesign = true;
 }
-inf_float::inf_float(float n) { new (this) inf_float((long double)n); }
-inf_float::inf_float(double n) { new (this) inf_float((long double)n); }
+inf_float::inf_float(int n) { new (this) inf_float(to_string(n)); }
+inf_float::inf_float(long long n) { new (this) inf_float(to_string(n)); }
+inf_float::inf_float(float n) { new (this) inf_float(to_string(n)); }
+inf_float::inf_float(double n) { new (this) inf_float(to_string(n)); }
 inf_float::inf_float(long double n) { new (this) inf_float(to_string(n)); }
 inf_float::inf_float(const char* str) { new (this) inf_float(std::string(str)); }
 inf_float::inf_float(std::string str)
@@ -117,29 +121,26 @@ inf_float::inf_float(std::string str)
         this->thesign = true;
     }
     ltrim(str); // 왼쪽 부분을 trim해서 불필요한 0을 제거해줌
-    if (str[0] == '.') { // .이 먼저 나오면 0을 더해줌
-        str = "0" + str;
-    }
     std::string buf = "";
-    unsigned int string_len = (unsigned int)str.size(), pos = 0;
-    for (int i = string_len - 1; i >= 0; i--) {
+    unsigned int str_len = (unsigned int)str.length(), pos = -1;
+    for (int i = str_len - 1; i >= 0; i--) {
         if (str[i] != '.')
             buf += str[i];
         else
             pos = i;
     }
-    if (string_len == 0) {
+    if (str_len == 0) {
         new (this) inf_float();
     } else if (this->digits == "0" && this->length == 1) {
         this->thesign = true;
     } else {
         this->digits = buf;
-        if (pos == 0) { // 소수점이 존재하지 않으면 digit len이 1 줄어들지 않고 precision_position이 0이 됨
-            this->length = string_len;
+        if (pos == -1) { // 소수점이 존재하지 않으면 digit len이 1 줄어들지 않고 precision_position이 0이 됨
+            this->length = str_len;
             this->precision_position = 0;
         } else {
-            this->length = string_len - 1;
-            this->precision_position = string_len - pos - 1;
+            this->length = str_len - 1;
+            this->precision_position = str_len - pos - 1;
         }
     }
 }
@@ -181,40 +182,6 @@ void FFT(std::vector<std::complex<double>>& f, bool inv = false)
             f[i] /= size;
         }
     }
-}
-std::pair<inf_int, inf_int> divmod(const inf_int& a, const inf_int& b)
-{
-    if (b == 0)
-        throw "div by zero";
-    if (a == 0)
-        return { 0, 0 };
-    if (a == b)
-        return { 1, 0 };
-    if (abs(a) < abs(b))
-        return { 0, inf_int(a) };
-    inf_int quotient, remainder(abs(a));
-    inf_int absb = abs(b);
-    long long n = a.length - b.length;
-    for (long long i = n; i >= 0; i--) {
-        long long powv = pow(10, i);
-        for (;;) {
-            inf_int tmp = absb * powv;
-            if (remainder >= tmp) {
-                quotient += powv;
-                remainder -= tmp;
-            } else
-                break;
-        }
-        if (remainder < absb)
-            break;
-    }
-    if (!a.thesign && !b.thesign)
-        return { quotient, -remainder };
-    if (a.thesign && !b.thesign)
-        return { -quotient, remainder };
-    if (!a.thesign && b.thesign)
-        return { -quotient, -remainder };
-    return { quotient, remainder };
 }
 inf_float& inf_float::operator=(const inf_float& a)
 {
@@ -263,18 +230,30 @@ bool operator>(const inf_float& a, const inf_float& b)
 bool operator<(const inf_float& a, const inf_float& b) { return ((a > b) || (a == b)) ? false : true; }
 bool operator>=(const inf_float& a, const inf_float& b) { return (a > b) || (a == b); }
 bool operator<=(const inf_float& a, const inf_float& b) { return (a < b) || (a == b); }
-inf_int operator+(const inf_int& a, const inf_int& b)
+inf_float operator+(const inf_float& a, const inf_float& b)
 {
-    if (a.thesign != b.thesign && abs(a) == abs(b)) {
+    if (a.thesign != b.thesign && a.digits == b.digits && a.precision_position == b.precision_position) {
         return 0;
     }
     if (a.thesign == true && b.thesign == true) {
+        unsigned int max_precision_position = max(a.precision_position, b.precision_position);
         std::string sa = a.digits;
         std::string sb = b.digits;
-        while ((int)sb.length() < a.length) {
+        if (a.precision_position < b.precision_position) {
+            for (int i = 0; i < b.precision_position - a.precision_position; i++) {
+                sa = '0' + sa;
+            }
+        } else {
+            for (int i = 0; i < a.precision_position - b.precision_position; i++) {
+                sb = '0' + sb;
+            }
+        }
+        int a_len = (int)sa.length();
+        int b_len = (int)sb.length();
+        while ((int)sb.length() < a_len) {
             sb += '0';
         }
-        while ((int)sa.length() < b.length) {
+        while ((int)sa.length() < b_len) {
             sa += '0';
         }
         std::string result = "";
@@ -288,17 +267,25 @@ inf_int operator+(const inf_int& a, const inf_int& b)
             result += (carry + '0');
         }
         reverse(result.begin(), result.end());
-        return inf_int(result);
+        int len = (int)result.length();
+        rtrim(result);
+        inf_float res(result);
+        res.length = (unsigned int)result.length();
+        res.precision_position = max_precision_position - (len - res.length);
+        ltrim(result);
+        res.length = (unsigned int)result.length();
+        return res;
     } else if (a.thesign == false && b.thesign == false) {
         return -(abs(a) + abs(b));
     } else {
-        inf_int c = b;
+        inf_float c(b);
         c.thesign = a.thesign;
         return a - c;
     }
 }
-inf_int operator-(const inf_int& a, const inf_int& b)
+inf_float operator-(const inf_float& a, const inf_float& b)
 {
+    /*
     if (a == b) {
         return 0;
     }
@@ -310,8 +297,8 @@ inf_int operator-(const inf_int& a, const inf_int& b)
         return -(abs(a) - abs(b));
     } else if (a.thesign == true && b.thesign == true) {
         bool flag = false;
-        inf_int aa(a);
-        inf_int bb(b);
+        inf_float aa(a);
+        inf_float bb(b);
         if (aa < bb) {
             aa = b;
             bb = a;
@@ -327,7 +314,7 @@ inf_int operator-(const inf_int& a, const inf_int& b)
         bb.digits[0]++;
         reverse(aa.digits.begin(), aa.digits.end());
         reverse(bb.digits.begin(), bb.digits.end());
-        inf_int result(inf_int(aa.digits) + inf_int(bb.digits));
+        inf_float result(inf_float(aa.digits) + inf_float(bb.digits));
         result.digits = result.digits.substr(0, result.length);
         reverse(result.digits.begin(), result.digits.end());
         result.digits.erase(result.digits.begin());
@@ -346,8 +333,9 @@ inf_int operator-(const inf_int& a, const inf_int& b)
         result.digits.substr(0, result.length);
         return result;
     }
+    */
 }
-inf_int operator*(const inf_int& a, const inf_int& b)
+inf_float operator*(const inf_float& a, const inf_float& b)
 {
     if (a == 0 || b == 0) {
         return 0;
@@ -363,11 +351,22 @@ inf_int operator*(const inf_int& a, const inf_int& b)
     } else if (a.thesign == false && b.thesign == false) {
         return abs(a) * abs(b);
     } else {
+        std::string sa = a.digits;
+        std::string sb = b.digits;
+        if (a.precision_position < b.precision_position) {
+            for (int i = 0; i < b.precision_position - a.precision_position; i++) {
+                sa = '0' + sa;
+            }
+        } else {
+            for (int i = 0; i < a.precision_position - b.precision_position; i++) {
+                sb = '0' + sb;
+            }
+        }
         std::vector<std::complex<double>> A, B, C;
-        for (auto i : a.digits) {
+        for (auto i : sa) {
             A.push_back(std::complex<double>(i - '0', 0));
         }
-        for (auto i : b.digits) {
+        for (auto i : sb) {
             B.push_back(std::complex<double>(i - '0', 0));
         }
         int num = 1;
@@ -401,113 +400,126 @@ inf_int operator*(const inf_int& a, const inf_int& b)
         int rsize = r.size();
         int i;
         for (i = rsize - 1; i >= 0 && r[i] == 0; i--) { }
-        std::string ans = "";
+        std::string result = "";
         if (i < 0) {
             return 0;
         } else {
             for (; i >= 0; i--) {
-                ans += (r[i] + '0');
+                result += (r[i] + '0');
             }
         }
-        return inf_int(ans);
+        int len = (int)result.length();
+        rtrim(result);
+        inf_float res(result);
+        res.length = (unsigned int)result.length();
+        res.precision_position = a.precision_position + b.precision_position - (len - res.length);
+        return res;
     }
 }
-inf_int operator/(const inf_int& a, const inf_int& b) { return divmod(a, b).first; }
-inf_int operator%(const inf_int& a, const inf_int& b) { return divmod(a, b).second; }
-inf_int operator+(const inf_int& a)
+inf_float operator/(const inf_float& a, const inf_float& b)
 {
-    inf_int tmp(a);
+    /*
+    if (b == 0)
+        throw "div by zero";
+    if (a == 0)
+        return { 0, 0 };
+    if (a == b)
+        return { 1, 0 };
+    if (abs(a) < abs(b))
+        return { 0, inf_float(a) };
+    inf_float quotient, remainder(abs(a));
+    inf_float absb = abs(b);
+    long long n = a.length - b.length;
+    for (long long i = n; i >= 0; i--) {
+        long long powv = pow(10, i);
+        for (;;) {
+            inf_float tmp = absb * powv;
+            if (remainder >= tmp) {
+                quotient += powv;
+                remainder -= tmp;
+            } else
+                break;
+        }
+        if (remainder < absb)
+            break;
+    }
+    if (!a.thesign && !b.thesign)
+        return { quotient, -remainder };
+    if (a.thesign && !b.thesign)
+        return { -quotient, remainder };
+    if (!a.thesign && b.thesign)
+        return { -quotient, -remainder };
+    return { quotient, remainder };
+    */
+}
+inf_float operator+(const inf_float& a)
+{
+    inf_float tmp(a);
     tmp.thesign = true;
     return tmp;
 }
-inf_int operator-(const inf_int& a)
+inf_float operator-(const inf_float& a)
 {
     if (a == 0) {
         return 0;
     }
-    inf_int tmp(a);
+    inf_float tmp(a);
     tmp.thesign = a.thesign ? false : true;
     return tmp;
 }
-inf_int& operator++(inf_int& a) { return a += 1; }
-inf_int operator++(inf_int& a, int)
+inf_float& operator++(inf_float& a) { return a += 1; }
+inf_float operator++(inf_float& a, int)
 {
-    inf_int tmp(a);
+    inf_float tmp(a);
     a += 1;
     return tmp;
 }
-inf_int& operator--(inf_int& a) { return a -= 1; }
-inf_int operator--(inf_int& a, int)
+inf_float& operator--(inf_float& a) { return a -= 1; }
+inf_float operator--(inf_float& a, int)
 {
-    inf_int tmp(a);
+    inf_float tmp(a);
     a -= 1;
     return tmp;
 }
-inf_int operator>>(const inf_int& a, const inf_int& b)
-{
-    inf_int tmp(a);
-    for (inf_int i = 0; i < b; i++) {
-        tmp /= 2;
-    }
-    return tmp;
-}
-inf_int operator<<(const inf_int& a, const inf_int& b)
-{
-    inf_int tmp(a);
-    for (inf_int i = 0; i < b; i++) {
-        tmp *= 2;
-    }
-    return tmp;
-}
-inf_int& operator+=(inf_int& a, const inf_int& b)
+inf_float& operator+=(inf_float& a, const inf_float& b)
 {
     a = a + b;
     return a;
 }
-inf_int& operator-=(inf_int& a, const inf_int& b)
+inf_float& operator-=(inf_float& a, const inf_float& b)
 {
     a = a - b;
     return a;
 }
-inf_int& operator*=(inf_int& a, const inf_int& b)
+inf_float& operator*=(inf_float& a, const inf_float& b)
 {
     a = a * b;
     return a;
 }
-inf_int& operator/=(inf_int& a, const inf_int& b)
+inf_float& operator/=(inf_float& a, const inf_float& b)
 {
     a = a / b;
     return a;
 }
-inf_int& operator%=(inf_int& a, const inf_int& b)
-{
-    a = a % b;
-    return a;
-}
-inf_int& operator>>=(inf_int& a, const inf_int& b)
-{
-    a = a >> b;
-    return a;
-}
-inf_int& operator<<=(inf_int& a, const inf_int& b)
-{
-    a = a << b;
-    return a;
-}
-inf_int abs(const inf_int& a) { return +a; }
+inf_float abs(const inf_float& a) { return +a; }
 std::ostream& operator<<(std::ostream& out, const inf_float& a)
 {
     if (a.thesign == false) {
         out << '-';
     }
+    if (a.length <= a.precision_position) {
+        out << "0.";
+        for (int i = 0; i < a.precision_position - a.length; i++) {
+            out << '0';
+        }
+    }
     for (int i = a.length - 1; i >= 0; i--) {
+        out << a.digits[i];
         if (i == a.precision_position) {
             out << ".";
             if (i == 0) {
                 out << "0";
             }
-        } else {
-            out << a.digits[i];
         }
     }
     return out;
